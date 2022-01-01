3d5d101d5fb8999237a5374a95cff341b522edf1
jemingyu1004 <94775977+jemingyu1004@users.noreply.github.com>
2022-01-01 18:01:21 +0900

    Add files via upload

---
+++ 봇.py
@@ -0,0 +1,7 @@
+import discord, asyncio, os
+from discord.ext import commands
+
+game = discord.Game("Primary Bot")
+bot = commands.Bot(command_prefix="!", status=discord.Status.online, activity=game)
+
+bot.run(ODk5MjM0OTEzODA5NzM1Njkw.YWvzxg.VsDxBGfbRb_nq5rbShDFj4eSkA0)
